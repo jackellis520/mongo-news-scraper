5bced275b7efdcfd58799924167a2732f1086caf
jackellis520 <54553984+jackellis520@users.noreply.github.com>
2019-11-02 22:12:26 -0500

    finished javascript for page

--- public/js/main.js
+++ public/js/main.js
@@ -60,3 +60,31 @@ $(function() {
 		$(button).toggleClass("waiting");
 		$(button).text("Scraping...");
 	});
+	//submit comment form
+	$("body").on("click", "#form-submit", function(event) {
+		event.preventDefault();
+		var articleId = $(".article").data("id");
+		$.ajax({
+			method: "POST",
+			url: "/api/" +articleId +"/comments",
+			data: {
+				author: $("#form-author").val(),
+				body: $("#form-body").val()
+			}
+		}).then(function() {
+			location.reload();
+		});
+	});
+
+	//delete comment
+	$("body").on("click", ".comment-delete", function(event) {
+		var commentId = $(this).data("id");
+
+		$.ajax({
+			method: "DELETE",
+			url: "/api/comments/" +commentId
+		}).then(function() {
+			location.reload();
+		});
+	});
+});
